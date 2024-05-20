2aab20c050a58a85b40bd4e02888881abe074457
manojkumarb001 <168207059+manojkumarb001@users.noreply.github.com>
2024-05-21 00:13:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
-document.getElementById('menu-btn').onclick = function() {
-    document.querySelector('.navbar').classList.toggle('active');
+let navbar = document.querySelector('.header .navbar');
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.add('active');
 }
 
-document.getElementById('nav-close').onclick = function() {
-    document.querySelector('.navbar').classList.remove('active');
+document.querySelector('#nav-close').onclick = () => {
+    navbar.classList.remove('active');
 }
 
-document.getElementById('search-btn').onclick = function() {
-    document.querySelector('.search-form').classList.toggle('active');
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.add('active');
 }
 
-document.getElementById('close-search').onclick = function() {
-    document.querySelector('.search-form').classList.remove('active');
+document.querySelector('#close-search').onclick = () => {
+    searchForm.classList.remove('active');
 }
 
-window.onscroll = function() {
+window.onscroll = () => {
+    navbar.classList.remove('active');
     if (window.scrollY > 0) {
         document.querySelector('.header').classList.add('active');
     } else {
         document.querySelector('.header').classList.remove('active');
     }
-}
+};
+
+window.onload = () => {
+    if (window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    } else {
+        document.querySelector('.header').classList.remove('active');
+    }
+};
